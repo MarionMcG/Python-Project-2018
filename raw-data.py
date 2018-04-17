ef2b883c84bdec8b4d7b6cfe6879bb6316170349
MarionMcG <g00364792@gmit.ie>
2018-04-17 20:45:55 +0100

    Update 17/04/2018

--- raw-data.py
+++ raw-data.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-#Marion McGowan 03/03/2018
-#Iris Dataset RAW
-
-print("Sepal Length Raw Data")
-
-with open ("data/iris-data.csv") as f:
-    for line in f:
-        print(", ",line.split(',')[0],end="")
-print()
-print("Sepal Width Raw Data")
-
-with open ("data/iris-data.csv") as f:
-    for line in f:
-        print(", ", line.split(',')[1],end="")
-print()
-print("Petal Length Raw Data")
-with open ("data/iris-data.csv") as f:
-    for line in f:
-        print(", ", line.split(',')[2],end="")
-print()
-print("Petal Width Raw Data")
-with open ("data/iris-data.csv") as f:
-    for line in f:
-        print(", " , line.split(',')[3],end="")
-=======
 #Marion McGowan 03/03/2018
 #Iris Dataset RAW
 #I wrote this program for my own use, it doesn't provide any relevant information.
@@ -49,5 +23,3 @@ print("Petal Width Raw Data")
 with open ("data/iris-data.csv") as f:
     for line in f:
         print(", " , line.split(',')[3],end="")
-        
->>>>>>> a9f7a864f5dbdc9de260002d0e8a0fd6b37b88d5
