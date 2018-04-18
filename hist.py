644ece25d5b1d5a5f04e59215ff40467860764d0
MarionMcG <g00364792@gmit.ie>
2018-04-18 23:13:28 +0100

    hist.py and associated imgs

---
+++ hist.py
@@ -0,0 +1,38 @@
+#Marion McGowan 
+#Distribution of four data sets: Sepal Length, Sepal Width, Petal Length and Petal Width
+
+import numpy as np
+import matplotlib.pyplot as pl
+data = np.genfromtxt('data/iris-data.csv', delimiter=',')
+firstcol = data[:,0]
+seccol =  data[:,1]
+thirdcol= data[:,2]
+fourcol= data[:,3]
+
+
+pl.hist(data[:,0], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Sepal Length')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.show()
+
+pl.hist(data[:,1], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Sepal Width')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.show()
+
+pl.hist(data[:,2], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Petal Length')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.show()
+
+pl.hist(data[:,3], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Petal Width')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.show()
+
+#Refered to this webpage: 
+#https://stackoverflow.com/questions/42741687/python-histogram-outline
