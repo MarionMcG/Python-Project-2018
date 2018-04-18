ffd8ef59cae6942d2cf8944423fd737c27f3b0a1
MarionMcG <g00364792@gmit.ie>
2018-04-19 00:28:42 +0100

    Historgrams graphed along the same x interval

--- hist.py
+++ hist.py
@@ -34,5 +34,35 @@ pl.xlabel('Length in cm')
 pl.ylabel('Number')
 pl.show()
 
+#Now taking the same distributions, I want to graph them along the same interval of the xaxis
+pl.hist(data[:,0], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Sepal Length')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.xticks([0,2, 4,6, 8, 10])
+pl.show()
+
+
+pl.hist(data[:,1], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Sepal Width')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.xticks([0,2, 4,6, 8, 10])
+pl.show()
+
+pl.hist(data[:,2], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Petal Length')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.xticks([0,2, 4,6, 8, 10])
+pl.show()
+
+pl.hist(data[:,3], edgecolor='k', linewidth=1.2, color ='salmon')
+pl.title('Petal Width')
+pl.xlabel('Length in cm')
+pl.ylabel('Number')
+pl.xticks([0,2, 4,6, 8, 10])
+pl.show()
+
 #Refered to this webpage: 
 #https://stackoverflow.com/questions/42741687/python-histogram-outline
